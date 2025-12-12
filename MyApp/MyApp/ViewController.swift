d5ca39e90aedca82b2ce8305b73bc95a02497dd4
Bharatsilavat96 <bharatsilavat96@gmail.com>
2025-12-13 02:21:19 +0530

    Now Project is created

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Bharat Shilavat on 13/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
